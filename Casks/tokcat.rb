8741494ba4f8a4ad0daf375770eb2f75f9e4ea8c
handlecusion <handlecu@gmail.com>
2026-05-04 10:50:18 +0900

    Strip quarantine and re-sign on cask install

--- Casks/tokcat.rb
+++ Casks/tokcat.rb
@@ -13,6 +13,12 @@ cask "tokcat" do
   app "Tokcat.app"
 
   postflight do
+    # Strip Gatekeeper quarantine and re-apply ad-hoc signature so the
+    # unsigned-but-distributed app can launch on first run.
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Tokcat.app"]
+    system_command "/usr/bin/codesign",
+                   args: ["--force", "--deep", "--sign", "-", "#{appdir}/Tokcat.app"]
     system_command "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister",
                    args: ["-f", "#{appdir}/Tokcat.app"]
   end
